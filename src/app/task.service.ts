bde2867a3eed76dcbda0a2c550b59f09857c1ef0
kotb1 <bodyyosef64@yahoo.com>
2022-01-02 03:06:49 +0200

    project661

---
+++ src/app/task.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './models/Todo';
+import { WebRequestService } from './web-request.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor(private webReqService: WebRequestService) {}
+  createList(title: string) {
+    // We want to send a web request to create a list
+    return this.webReqService.post({ title });
+  }
+  getTasks(){
+    return this.webReqService.get();
+  }
+  deleteTask(task:Todo)
+  {
+    return this.webReqService.delete(`${task._id}`);
+  }
+  complete(task:Todo)
+  {
+    return this.webReqService.patch(`${task._id}`,{completed : !task.completed});
+  }
+  }
